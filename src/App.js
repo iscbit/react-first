0febd6fdb71ff54438aabae922988930811f421c
iscbit <iscbit@gmail.com>
2017-09-25 23:25:30 +0200

    update

--- src/App.js
+++ src/App.js
@@ -4,24 +4,17 @@ import './App.css';
 
 class App extends Component {
     render() {
-        return ( <
-            div className = "App" >
-            <
-            div className = "App-header" >
-            <
-            img src = { logo }
-            className = "App-logo"
-            alt = "logo" / >
-            <
-            h2 > Welcome to React < /h2> <
-            /div> <
-            p className = "App-intro" >
-            <
-            button > csatlakozás < /button> <
-            /p> <
-            /div>
-        );
+        return (
+            <div className="App">
+              <div className="App-header">
+                <img src={ logo } className="App-logo" alt="logo" />
+                <h2> Welcome to React </h2>
+              </div>
+              < p className="App-intro">
+                < button> csatlakozás </button>
+                  </p>
+            </div>
+            );
     }
 }
-
 export default App;
